b850f7c3f7e33e147202b305035e274654eee3dd
Rane Wallin <rane@ranewallin.com>
2019-02-07 12:37:59 +0000

    Complete MVP

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -12,7 +12,7 @@ class TabLink {
 
     // Using the custom data attribute get the associated Item element
     // this.itemElement;t
-    this.itemElement = document.querySelector(`.tabs-link[data-tab='${this.data}']`);
+    this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
 
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
